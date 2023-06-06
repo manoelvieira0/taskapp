f9073870a7d39d3a28e5bb4afd19a9ed1b9506e0
Manoel Vieira <netinhovieira1801@gmail.com>
2023-06-06 20:18:45 -0300

    Estilizando

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Manoel Vieira</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex: 1,
+      backgroundColor: "#131016",
+      padding: 24
+    }}>
+      <Text style={{
+        color: "#FFF",
+        fontSize: 24,
+        fontWeight: "bold"
+      }}>
+        Nome do evento
+      </Text>
+      <Text style={{
+        color: "#6b6b6b",
+        fontSize: 16
+      }}>
+        Sexta, 4 de novembro de 2023
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ccc',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

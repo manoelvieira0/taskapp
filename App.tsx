10d4b0ef8ecc1e6d09c73ef129aee6aca9af019a
Manoel Vieira <netinhovieira1801@gmail.com>
2023-06-06 21:30:21 -0300

    Input de dados e organizando o projeto

--- App.tsx
+++ App.tsx
@@ -1,31 +1,8 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>
-        Nome do evento
-      </Text>
-      <Text style={styles.eventDate}>
-        Sexta, 4 de novembro de 2023
-      </Text>
-    </View>
+    <Home />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#131016",
-    padding: 24
-  },
-  eventName: {
-    color: "#FFF",
-    fontSize: 24,
-    fontWeight: "bold",
-    marginTop: 48
-  }, eventDate: {
-    color: "#6b6b6b",
-    fontSize: 16
-  }
-});

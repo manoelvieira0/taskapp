35a383c2cd719e38d897ba5cbcb919b88bc2eeb1
Manoel Vieira <netinhovieira1801@gmail.com>
2023-06-07 09:00:26 -0300

    Botão adicionar

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,13 @@
-import { Text, View, TextInput } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 
 import { styles } from "./styles";
 
 export function Home() {
+
+    function handleParticipantAdd(){
+        console.log("Você clicou no botão adicionar")
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.eventName}>
@@ -18,6 +23,10 @@ export function Home() {
             placeholder="Nome do participante"
             placeholderTextColor="#6b6b6b"
             />
+
+            <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+                <Text style={styles.buttonText}>+</Text>
+            </TouchableOpacity>
         </View>
     );
 }

d580e6b26a1679cfeb7dce7d12447a7b56339ba9
Manoel Vieira <netinhovieira1801@gmail.com>
2023-06-08 20:39:00 -0300

    Removendo participante da lista

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -22,7 +22,7 @@ export function Home() {
         Alert.alert("Remover", `Deseja remover o participante ${name}?`, [
             {
                 text: "Sim",
-                onPress: () => Alert.alert("Deletado!")
+                onPress: () => setParticipants(prevState => prevState.filter(participant => participant !== name))
             },
             {
                 text: "Não",
